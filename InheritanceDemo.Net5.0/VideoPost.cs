8bef38fbd9da01f6ff2e6e703cf3742a1fa51531
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 10:10:25 -0400

    Added constructors to the VideoPost class

--- InheritanceDemo.Net5.0/VideoPost.cs
+++ InheritanceDemo.Net5.0/VideoPost.cs
@@ -11,5 +11,21 @@ namespace InheritanceDemo.Net5._0
         //property
         public string VideoURL { get; set; }
         public int Length { get; set; }
+
+        //constructor
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, int length, bool isPublic)
+        {
+            //The following properties and the GetNextID method are inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUserName = sendByUsername;
+            this.IsPublic = isPublic;
+
+            //Property ImageURL is a member of the ImagePost, but not of Post.
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
     }
 }
