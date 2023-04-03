bc82f352f3bb0a1030415bfbda767b427486cd93
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 09:51:34 -0400

    Added two contructors to the ImagePost Class

--- InheritanceDemo.Net5.0/ImagePost.cs
+++ InheritanceDemo.Net5.0/ImagePost.cs
@@ -12,5 +12,13 @@ namespace InheritanceDemo.Net5._0
         //property
         public string ImageURL { get; set; }
 
+        //constructor
+        public ImagePost() { }
+
+        public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
+        {
+            this.ID = GetNextID();
+        }
+
     }
 }
