1772e5dbcc4dc1909d880d94b889aa854df58e76
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 09:14:14 -0400

    Added a ImageURL property to the ImagePost class

--- InheritanceDemo.Net5.0/ImagePost.cs
+++ InheritanceDemo.Net5.0/ImagePost.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace InheritanceDemo.Net5._0
 {
+    //ImagePost derives from Post and adds a property (ImageURL) and two constructors
     internal class ImagePost : Post
     {
+        //property
+        public string ImageURL { get; set; }
+
     }
 }
